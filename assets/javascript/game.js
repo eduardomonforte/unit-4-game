330866cdd25038eb9e35a0dd799cceb633b39657
Eduardo Monforte <eduardomonforte@gmail.com>
2018-09-16 00:21:21 -0500

    Created click events for all gem ores.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -34,13 +34,13 @@ console.log("I'm linked!");
 
         checkValues();
 
-        console.log("I should be running.")
+        console.log("I'll run each time the values have to be reset.")
     }
 
-    // This functions makes sure that each gem has a different value.
+    // This function makes sure that each gem has a different value.
     // Thanks to Charlie Acevedo for his help on this one!
     function checkValues() {
-        if(diamondValue!==emeraldValue&&diamondValue!==goldValue&&diamondValue!==redstoneValue&&emeraldValue!==goldValue&&emeraldValue!==redstoneValue&&goldValue!==redstoneValue) {
+        if(diamondValue !== emeraldValue && diamondValue !== goldValue && diamondValue !== redstoneValue && emeraldValue !== goldValue && emeraldValue !== redstoneValue && goldValue !== redstoneValue) {
             clickCrystals();
         }
         else {
@@ -49,6 +49,26 @@ console.log("I'm linked!");
     }
 
     function clickCrystals() {
+
+        $("#diamond-ore").on("click", function() {
+            currentScore += diamondValue;
+            $("#current-score").html(currentScore);
+        });
+
+        $("#emerald-ore").on("click", function() {
+            currentScore += emeraldValue;
+            $("#current-score").html(currentScore); 
+        });
+
+        $("#gold-ore").on("click", function() {
+            currentScore += goldValue;
+            $("#current-score").html(currentScore);
+        });
+
+        $("#redstone-ore").on("click", function() {
+            currentScore += redstoneValue;
+            $("#current-score").html(currentScore);
+        });
 
     }
 
