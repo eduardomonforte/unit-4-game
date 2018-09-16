9b65312bec7b7726a8799e73ec8adb60f258ac52
Eduardo Monforte <eduardomonforte@gmail.com>
2018-09-15 23:21:49 -0500

    Declared variables and linked scores to the JavaScript file.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,19 @@
+$(document).ready(); {
+
 console.log("I'm linked!");
 
+    var matchThis = 0;
+    var currentScore = 0;
+    var diamondValue = 0;
+    var emeraldValue = 0;
+    var goldValue = 0;
+    var redstoneValue = 0;
+    var wins = 0;
+    var loses = 0;
 
+    $("#match-this").html(matchThis);
+    $("#current-score").html(currentScore);
+    $("#times-won").html(wins);
+    $("#times-lost").html(loses);
+
+}
